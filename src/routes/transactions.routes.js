d40cab8929b00806dae1559e60d0fbb55722f21e
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-07 17:00:16 -0300

    feat(route): import and use multer to upload files

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import TransactionController from "../controllers/transactions.control";
 import Transaction from "../models/transactions.model";
+import cnabToJson from "../middlewares/json-converter.middle";
 
+import multer from "multer";
+const upload = multer({ dest: 'uploads/' })
 const router = express.Router();
 const transactions = new TransactionController(Transaction);
 
 router.get('/', transactions.getAllTransactions);
-router.post('/save', transactions.saveTransaction);
+router.post('/save', upload.single('file'), cnabToJson.convertCnabToJson, transactions.saveTransaction);
 
 export default router;
