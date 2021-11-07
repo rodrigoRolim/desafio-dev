fc60dc294010bca6b28698a68c11573ed3c06b30
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-07 16:59:27 -0300

    feat: set header and enconded url as a tru

--- src/app.js
+++ src/app.js
@@ -4,12 +4,18 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
