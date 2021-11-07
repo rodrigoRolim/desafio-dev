d2489a9005c092c9ce476c64174854dd5972f705
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-07 20:25:20 -0300

    refactor(model): format the string

--- src/models/transactions.model.js
+++ src/models/transactions.model.js
@@ -1,29 +1,19 @@
 import db from "./db";
 
-export default class Transactions {
+import { Inserts } from "../helpers/insert-mulple";
+
+class Transactions {
   constructor() {
 
   }
   async save(datas) {
-    return await db.query('INSERT INTO transactions(' + 
-      'type_transaction,' +
-      'transaction_value,' +
-      'cpf,' +
-      'card_number,' +
-      'date_created,' +
-      'hour_created,' +
-      'store_own,' +
-      'store_name) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *', [
-      datas.value, 
-      datas.cpf,
-      datas.cardNumber,
-      datas.date,
-      datas.hour,
-      datas.storeName,
-      datas.typeTransaction
-    ])
+    const values = Inserts('${type_transaction}, ${transaction_value}, ${cpf}, ${card_number}, ${date_created}, ${hour_created}, ${store_own}, ${store_name}', datas);
+    
+    return await db.none('INSERT INTO transactions (type_transaction,transaction_value,cpf,card_number,date_created,hour_created,store_own,store_name) VALUES $1', values);
   }
   async getAll() {
     return await db.query('SELECT * FROM transactions');
   }
 }
+
+export default Transactions;
