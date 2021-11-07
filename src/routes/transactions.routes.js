6c27c6fa9b656937010425e10af2a31ec6275304
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-07 15:04:03 -0300

    refactor(route): rename function of the controller

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 const transactions = new TransactionController(Transaction);
 
 router.get('/', transactions.getAllTransactions);
-router.post('/save', transactions.save);
+router.post('/save', transactions.saveTransaction);
 
 export default router;
