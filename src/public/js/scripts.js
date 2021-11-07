d947793ad579f98de7b6123ad4348ebf76699745
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-07 15:26:12 -0300

    feat(public): upload cnab file

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,6 +1,18 @@
 (function() {
-  const file = document.getElementById("file");
-  file.addEventListener('change', (e) => {
+  async function upload(file) {
+    let formData = new FormData();
+    formData.append("file", file);
+    await fetch('/transactions/save', {
+      method: 'POST',
+      body: formData
+    })
+  }
 
+  const file = document.getElementById("file");
+
+  file.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    upload(file);
   })
+
 })()
