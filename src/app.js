583c0eca9f374e28a377e3eadc78f12388fb85de
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-07 14:26:42 -0300

    chore: config parse and encoder of the url

--- src/app.js
+++ src/app.js
@@ -7,3 +7,8 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
