92193e1ca5b375c7cdc9ad6d0ce245e07a390eee
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-07 20:27:02 -0300

    refactor(route): rename variables

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -1,14 +1,17 @@
 import express from "express";
 import TransactionController from "../controllers/transactions.control";
-import Transaction from "../models/transactions.model";
+import Transactions from "../models/transactions.model";
 import cnabToJson from "../middlewares/json-converter.middle";
 
 import multer from "multer";
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: 'uploads/' });
+
 const router = express.Router();
-const transactions = new TransactionController(Transaction);
+const transactionsModel = new Transactions();
+
+const transactions = new TransactionController(transactionsModel);
 
 router.get('/', transactions.getAllTransactions);
-router.post('/save', upload.single('file'), cnabToJson.convertCnabToJson, transactions.saveTransaction);
+router.post('/save', upload.single('file'), cnabToJson.convertCnabToJson, (req, res) => transactions.saveTransaction(req, res));
 
 export default router;
