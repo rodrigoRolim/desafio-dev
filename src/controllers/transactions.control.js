59c9df85f9f4eae25e2a772d8fb8207b997764f7
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-07 18:10:32 -0300

    feat(controller): save a transaction

--- src/controllers/transactions.control.js
+++ src/controllers/transactions.control.js
@@ -1,11 +1,17 @@
-export default class TransactionController {
-  constructor(Transaction) {
-    this.Transaction = Transaction;
+class TransactionController {
+  
+  constructor(Transactions) {
+    this.transactions = Transactions;
   }
-  saveTransaction() {
-
+  
+  saveTransaction(req, res) {
+   
+    return this.transactions.save(req.records)
+      .then(resp => res.send(resp))
+      .catch(err => res.status(500).send(err)); 
   }
   getAllTransactions(req, res) {
     
   }
 }
+export default TransactionController;
