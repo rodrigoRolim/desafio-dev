e71e5c3c8dae70bea7f926d98fd7e8b889ce7911
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-07 20:26:36 -0300

    feat(public): send file to server when file is uploaded

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,16 +1,18 @@
 (function() {
   async function upload(file) {
+
     let formData = new FormData();
-    formData.append("file", file);
-    await fetch('/transactions/save', {
+    formData.append("file", file, "cnab.txt");
+
+    const response = await fetch('/transactions/save', {
       method: 'POST',
       body: formData
-    })
+    });
   }
 
   const file = document.getElementById("file");
 
-  file.addEventListener('change', (e) => {
+  file.addEventListener('change', function (e) {
     const file = e.target.files[0];
     upload(file);
   })
