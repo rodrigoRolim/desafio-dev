d2e058e2aa03866350ddbd238900c1a6b17f5da9
rodrigoRolim <rodrigorolimveras92@gmail.com>
2021-11-07 21:19:02 -0300

    feat: config dotenv

--- src/models/db.js
+++ src/models/db.js
@@ -1,11 +1,18 @@
 import { Pool } from "pg";
+import dotenv from 'dotenv';
+
+const result = dotenv.config()
+
+if (result.error) {
+  throw result.error
+}
 
 const pool = new Pool({
-  user: 'root',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'postgres',
-  port: 5432
+  user: process.env.USER_DB,
+  host: process.env.HOST_DB,
+  database: process.env.NAME_DB,
+  password: process.env.PASSWORD_DB,
+  port: process.env.PORT_DB
 });
 
 export default {
